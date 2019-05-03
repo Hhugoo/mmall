9a90a4bbcdd98016f0e1fcd99d0daf80bdc07be3
hugoo <hugoo@hugoodeMBP.lan>
2019-05-03 13:36:20 +0800

    add session filter

---
+++ src/main/java/com/mmall/controller/common/SessionExpireFilter.java
@@ -0,0 +1,44 @@
+package com.mmall.controller.common;
+
+import com.mmall.common.Const;
+import com.mmall.common.RedisPool;
+import com.mmall.pojo.User;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisPoolUtil;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class SessionExpireFilter implements Filter{
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest)servletRequest;
+
+        String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+        if (StringUtils.isNotEmpty(loginToken)) {
+            //如果logintoken不为空或""，就去拿user信息
+            String userJsonStr = RedisPoolUtil.get(loginToken);
+            User user = JsonUtil.stringToObj(userJsonStr, User.class);
+            if (user != null) {
+                //如果user不为空，重置session时间，即调用expire命令
+                RedisPoolUtil.expire(loginToken, Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
+            }
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
